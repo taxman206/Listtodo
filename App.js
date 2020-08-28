8cc253e44e638bfefbcdd80f76a56f8eb90f2332
taxman206 <7835@ait.nsw.edu.au>
2020-08-29 03:32:03 +1000

    first comit

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,SafeAreaView} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+render(){
+	return(
+	<SafeAreaView> //ENSURES SCREEN IS FITTED
+
+	</SafeAreaView>
+	
+	)
+}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles=StyleSheet.create({
+	main:{
+		paddingHorizontal:10,
+	}
+})
---
+++ components/item.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Text,View,StyleSheet} from 'react-native';
+
+export const Item = (props)=> {
+	return(
+		<View style ={itemStyles.item>
+		<Text style={itemsStyles.text}>{props.category}</Text>
+		<Text style={itemStyles.text}>{props.amount}</Text>
+		</View>
+	)
+}
+
+const itemStyles = StyleSheet.create({
+	item:{
+		padding:10,
+		display:'flex',
+		flexDirection:'row',
+		justifyContent:'space-between'
+	},
+	text:{
+		fontSize:16,
+		color:'black'
+	}
+
+})
